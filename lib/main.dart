b61ecf1cb5393bb347a94798f4f7bde36324bd71
Muhammad Azzam Izzuddin <azzamizzuddin123@gmail.com>
2022-11-21 09:50:31 +0700

    potrait screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:app_ilmu_tajwid/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp]).then((_) {
+    runApp(
+      const MyApp(),
+    );
+  });
 }
 
 class MyApp extends StatelessWidget {
